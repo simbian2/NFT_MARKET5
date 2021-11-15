c5180232932ac58baa21533c0418f9737b88cb72
이진우 <binarywoo@gmail.com>
2021-11-15 18:35:44 +0900

    update UI

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,24 +1,28 @@
-import SidenavHeader from './SidenavHeader'
-import NavbarCollapse from './NavbarCollapse'
-import SidenavFooter from './SidenavFooter'
+import SidenavHeader from './SidenavHeader';
+import NavbarCollapse from './NavbarCollapse';
+import SidenavFooter from './SidenavFooter';
 
-import {AsidelogoCt} from '../../utils/allImgs'
-import {hideShowSidebar} from '../../utils'
-import data from '../../data/data-components/data-Aside.json'
+import { AsidelogoCt } from '../../utils/allImgs';
+import { hideShowSidebar } from '../../utils';
+import data from '../../data/data-components/data-Aside.json';
 
 const AsideContainer = () => {
-
   return (
     <>
-      <aside className="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-left ms-3" id="sidenav-main">
-        <SidenavHeader AsidelogoCt={AsidelogoCt} hideShowSidebar={hideShowSidebar} />
+      <aside
+        className="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-left ms-3"
+        id="sidenav-main"
+      >
+        <SidenavHeader
+          AsidelogoCt={AsidelogoCt}
+          hideShowSidebar={hideShowSidebar}
+        />
         <hr className="horizontal dark mt-0" />
         <NavbarCollapse data={data} />
         <SidenavFooter />
       </aside>
     </>
   );
-}
+};
 
 export default AsideContainer;
-
--- src/layouts/Head/Navbar/SecNavItem/index.jsx
+++ src/layouts/Head/Navbar/SecNavItem/index.jsx
@@ -1,25 +1,32 @@
-import { NavLink } from "react-router-dom";
-import ITag from '../../../../components/ITag'
+import { NavLink } from 'react-router-dom';
+import ITag from '../../../../components/ITag';
 
-function SecNavItem({hideShowSidebar}){
+function SecNavItem({ hideShowSidebar }) {
+  return (
+    <>
+      <li className="nav-item d-flex align-items-center">
+        <NavLink to="/" className="btn btn-white bg-light mb-0 w-100">
+          Connect Wallet
+        </NavLink>
+        {/* <NavLink
+          to="/signin"
+          className="nav-link text-body font-weight-bold px-0"
+        >
+          <span className="d-sm-inline text-body d-none mr-10">Sign In</span>
+        </NavLink> */}
+      </li>
 
-  return(
-  	<>
-	    <li className="nav-item d-flex align-items-center">
-	      <NavLink to="/signin" className="nav-link text-body font-weight-bold px-0">
-	        <span className="d-sm-inline text-body d-none mr-10">Sign In</span>
-	      </NavLink>
-	    </li>
-
-	    <li className="nav-item d-xl-none px-3 d-flex align-items-center">
-	      <div className="sidenav-toggler-inner" onClick={hideShowSidebar}>
-	        {Array(3).fill().map((item , i) => (
-	          <ITag ClassName="sidenav-toggler-line" key={i} />
-	        ))}
-	      </div>
-	    </li>
+      <li className="nav-item d-xl-none px-3 d-flex align-items-center">
+        <div className="sidenav-toggler-inner" onClick={hideShowSidebar}>
+          {Array(3)
+            .fill()
+            .map((item, i) => (
+              <ITag ClassName="sidenav-toggler-line" key={i} />
+            ))}
+        </div>
+      </li>
     </>
-  )
+  );
 }
 
-export default SecNavItem
+export default SecNavItem;
